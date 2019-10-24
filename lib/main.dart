1ac1f1333e070fc15c73f940e72499f2c65ad216
Leandre <leajapo@gmail.com>
2019-10-24 12:52:14 +0200

    feat:added chart for expenses

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:expenses/widgets/new_transaction.dart';
-import 'package:expenses/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
 
 import './models/transaction.dart';
 import './widgets/transaction_list.dart';
+import './widgets/chart.dart';
+import './widgets/new_transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -50,6 +50,18 @@ class _MyHomePageState extends State<MyHomePage> {
     //     id: 't2', title: 'Tomatoes', amount: 10.99, date: DateTime.now())
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where(
+      (tx) {
+        return tx.date.isAfter(
+          DateTime.now().subtract(
+            Duration(days: 7),
+          ),
+        );
+      },
+    ).toList();
+  }
+
   void _addNewTransaction(String title, double amount) {
     final newTx = Transaction(
       title: title,
@@ -89,16 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              height: 150,
-              margin: EdgeInsets.all(10),
-              child: Card(
-                color: Colors.blue,
-                elevation: 10,
-                child: Text('Chart'),
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
